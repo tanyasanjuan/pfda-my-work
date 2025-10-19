8d5b7456849f691e49dc1b4342840fc4d27e7fd5
Tanya <tanya.sanjuan@gmail.com>
2025-10-19 18:19:53 +0100

    header separated by a line

--- Week02/lab01.py
+++ Week02/lab01.py
@@ -6,10 +6,20 @@ import csv
 
 filename = 'data.csv'
 datadir = '/Users/tanya/OneDrive/Documentos/ATU/Dev/pands/pfda-my-work/Week02/'
-
+"""
 with open (datadir + filename, 'rt') as fp:
     reader = csv.reader(fp, delimiter=',')
     for line in reader:
         print(line)
 # this will print each line as a list
-
+"""
+# modifiying the program to deal with the header line separately
+with open (datadir + filename, 'rt') as fp:
+    reader = csv.reader(fp, delimiter=',')
+    linecount = 0
+    for line in reader:
+        if not linecount: # first row ie header row
+            print(f"{line}\n-------------------")
+        else:
+            print(line)
+        linecount += 1
