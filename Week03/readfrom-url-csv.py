adffd6fd9d23d49c374571848e96720dd7241e2b
Tanya <tanya.sanjuan@gmail.com>
2025-10-29 21:07:07 +0000

    using Json to diplay the data.

--- Week03/readfrom-url-csv.py
+++ Week03/readfrom-url-csv.py
@@ -10,4 +10,10 @@ print(df.head())  # Display the first few rows of the DataFrame
 # For different protocol, (we need to pip install S3Fs)
 url = "s3://noaa-gsod-pds/2020/72278023183.csv"
 df_s3 = pd.read_csv(url)
-print(df_s3.head())  # Display the first few rows of the DataFrame from S
+print(df_s3.head())  # Display the first few rows of the DataFrame from S3
+
+# Using Json will display the data in a different format
+url_json = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=wind_speed_10m,rain&past_days=3&forecast_days=1&format=json"
+df_json = pd.read_json(url_json)
+print(df_json.head())  # Display the first few rows of the DataFrame from JSON
+# results are in a list format.
