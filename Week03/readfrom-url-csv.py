836476a295cb8f43e532ab9d42311ec63c9b6adb
Tanya <tanya.sanjuan@gmail.com>
2025-10-31 18:43:55 +0000

    Writing data: To write it out an excel file

--- Week03/readfrom-url-csv.py
+++ Week03/readfrom-url-csv.py
@@ -12,6 +12,10 @@ url = "s3://noaa-gsod-pds/2020/72278023183.csv"
 df_s3 = pd.read_csv(url)
 print(df_s3.head())  # Display the first few rows of the DataFrame from S3
 
+# Write it out to an Excel file
+excel_filename = 'weather_data.xlsx'
+df.to_excel(excel_filename, index=False)
+
 # Using Json will display the data in a different format
 url_json = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=wind_speed_10m,rain&past_days=3&forecast_days=1&format=json"
 df_json = pd.read_json(url_json)
